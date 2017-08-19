281c1cc2f152218ab1922b0068796ed438e5d56e
JonathanPool <jonathan.pachacama@epn.edu.ec>
2017-08-19 06:29:07 -0500

    arreglo responsi

--- angular/zapatosFrontend/src/app/inicio/inicio.component.ts
+++ angular/zapatosFrontend/src/app/inicio/inicio.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  titulo:String ="Bienvenido COCCOLATI Company";
+  titulo:String ="shop shoes";
   constructor() { }
 
   ngOnInit() {
